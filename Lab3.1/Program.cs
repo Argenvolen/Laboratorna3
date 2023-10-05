e8da50f1c9022946103a8613356095d1b4892a80
Argenvolen <39115205+Argenvolen@users.noreply.github.com>
2023-10-05 19:48:44 +0300

    Add files via upload

---
+++ Lab3.1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Xml;
+
+class Logs
+{
+    public static List<Event> EventList = new List<Event>();
+    public static void Add(Event e)
+    {
+        EventList.Add(e);
+    }
+    public static void Show()
+    {
+        foreach (Event i in EventList)
+        {
+            i.PrintToConsole();
+        }
+    }
+}
+
+class Event
+{
+    public string Date { get; private set; }
+    public string Result { get; private set; }
+    public string IpFrom { get; private set; }
+    public string Method { get; private set; }
+    public string UrlTo { get; private set; }
+    public string Response { get; private set; }
+
+    public Event(string date, string result, string ipFrom, string method, string urlTo, string response)
+    {
+        Date = date;
+        Result = result;
+        IpFrom = ipFrom;
+        Method = method;
+        UrlTo = urlTo;
+        Response = response;
+    }
+
+    public void PrintToConsole()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine($"Дата: {Date}");
+        Console.WriteLine($"Результат: {Result}");
+        Console.WriteLine($"IP-адреса: {IpFrom}");
+        Console.WriteLine($"Метод: {Method}");
+        Console.WriteLine($"URL: {UrlTo}");
+        Console.WriteLine($"Відповідь: {Response}");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        XmlDocument xml = new XmlDocument();
+        xml.Load(@"D:\XMLFile2.xml");
+        foreach (XmlNode node in xml.DocumentElement)
+        {
+            string date = node.Attributes[0].InnerText;
+            string result = node.Attributes[1].InnerText;
+            string ipFrom = node["ip-from"].InnerText;
+            string method = node["method"].InnerText;
+            string urlTo = node["url-to"].InnerText;
+            string response = node["response"].InnerText;
+            Logs.Add(new Event(date, result, ipFrom, method, urlTo, response));
+        }
+        Logs.Show();
+    }
+}
+
---
+++ Lab3.2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Xml;
+
+class Note
+{
+    public string Id { get; private set; }
+    public string Date { get; private set; }
+    public string Time { get; private set; }
+    public string Subject { get; private set; }
+    public string Text { get; private set; }
+    public string Tel { get; private set; }
+
+    public Note(string id, string date, string time, string subject, string text, string tel)
+    {
+        Id = id;
+        Date = date;
+        Time = time;
+        Subject = subject;
+        Text = text;
+        Tel = tel;
+    }
+    public void PrintToConsole()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine($"ID: {Id}");
+        Console.WriteLine($"Дата: {Date}");
+        Console.WriteLine($"Час: {Time}");
+        Console.WriteLine($"Тема: {Subject}");
+        Console.WriteLine($"Текст: {Text}");
+        Console.WriteLine($"Телефон: {Tel}");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        XmlDocument xml = new XmlDocument();
+        xml.Load(@"D:\XMLFile1.xml");
+
+        foreach (XmlNode node in xml.DocumentElement)
+        {
+            string id = node.Attributes["id"].InnerText;
+            string date = node.Attributes["date"].InnerText;
+            string time = node.Attributes["time"].InnerText;
+            string subject = node["subject"].InnerText;
+            string text = node["text"].InnerText;
+            string tel = node["text"]["tel"].InnerText;
+
+            Note note = new Note(id, date, time, subject, text, tel);
+            note.PrintToConsole();
+        }
+    }
+}
